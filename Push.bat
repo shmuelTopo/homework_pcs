032e444807fa10dd539b79339aa60514a4f535bc
toporowitch <4158847@gmail.com>
2021-06-11 11:48:19 -0400

    Day 51

--- Push.bat
+++ Push.bat
@@ -1,6 +1,8 @@
-set /p massage=Enter some text: 
+set /p massage=Enter the massage to GitHub: 
 git pull
 git add .
 git commit -m "%massage%"
 git push
+TIMEOUT /T 10
+set /p massage=Press Enter to exit: 
  
