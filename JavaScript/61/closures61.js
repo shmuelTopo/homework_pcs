f6eff333878081ed538d33658c60e6cf2634641c
shmuel <4158847@gmail.com>
2021-08-11 14:08:20 -0400

    Homework 61

--- JavaScript/61/closures61.js
+++ JavaScript/61/closures61.js
@@ -1,1 +1,23 @@
-// Will fill up later today, before class
+'use strict';
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+console.log(`3 * 4 = ${multiply(3, 4)}`);
+
+function getMultiplier() {
+    return (num1, num2) => num1 * num2;
+}
+
+let multiply2 = getMultiplier();
+console.log(`5 * 4 = ${multiply2(5, 4)}`);
+
+function getMultiplierByNumber(multiplier) {
+    return (num) => multiplier * num;
+}
+
+let weeksToDaysCalculator = getMultiplierByNumber(7);
+let weeks = 8;
+console.log(`There are ${weeksToDaysCalculator(weeks)} days in ${weeks} weeks`);
+
