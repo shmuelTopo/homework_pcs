c5a8135475d69264d0c26b589ecbe4407fa7380c
Shmuel <4158847@gmail.com>
2021-09-19 18:38:10 -0400

    Homework 68

--- JavaScript/67/messageBox.js
+++ JavaScript/67/messageBox.js
@@ -3,27 +3,24 @@ window.shmuel = window.shmuel || {};
 window.shmuel.messageBox = (function () {
     'use strict';
 
+    const offsetIncrement = 10;
+    let leftOffset = -150;
+    let topOffset = -125;
+    const width = 300;
+    const height = 250;
+    let nextZIndex = 1;
+    const model = document.createElement('div');
+    model.classList.add('model');
+    document.body.appendChild(model);
+    const head = document.getElementsByTagName('HEAD')[0];
+    
+    //Add the Css from JavaScript
+    const customCss = document.createElement('link');
+    customCss.rel = 'stylesheet';
+    customCss.href = 'messageBox.css';
+    
 
-    //Adding the Boostrap css and the custom css style sheet from the Dom
-    // document.getElementsByTagName("head")[0].insertAdjacentHTML(
-    //     'beforeend',
-    //     '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css">'
-    //     );
-
-
-    // document.getElementsByTagName("head")[0].insertAdjacentHTML(
-    //     "beforeend",
-    //     '<link rel="stylesheet" href="messageBox.css" />');
-
-
-    //     alert('Hello');
-
-    // Get HTML head element
-    let head = document.getElementsByTagName('HEAD')[0];
-    let link = document.createElement('link');
-    link.rel = 'stylesheet';
-    link.href = 'https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css';
-    head.appendChild(link);
+    head.appendChild(customCss);
 
     function get(id) {
         return document.getElementById(id);
@@ -37,52 +34,89 @@ window.shmuel.messageBox = (function () {
         element.addEventListener(eventType, callback);
     }
 
-    const bidenAlert = get('bidenAlert');
-    const trumpAlert = get('trumpAlert');
-    const messageBox = get('messageBox');
-
-
-
-    function makeMessageBox(msg) {
+    function showMessageBox(msg, isModel, options, callback) {
+        if(isModel){
+            model.style.display = 'block';
+            model.style.zIndex = nextZIndex++;
+        }
+        
         const messageBoxDiv = document.createElement('div');
+        messageBoxDiv.classList.add('box');
+        messageBoxDiv.style.width = `${width}px`;
+        messageBoxDiv.style.height = `${height}px`;
+        messageBoxDiv.style.position = 'absolute';
+        messageBoxDiv.style.top = '50%';
+        messageBoxDiv.style.left = '50%';
+        messageBoxDiv.style.marginTop = `${topOffset}px`;
+        messageBoxDiv.style.marginLeft = `${leftOffset}px`;
+        messageBoxDiv.style.zIndex = nextZIndex++;
 
-        const messageBoxHtml = `
-            <div id="messageBox" class="alert alert-success centered">
-                    <h2 class="text-center">This is an Alert</h2>
-                    <p id="messageText">${msg}</p>
-                    <div class="text-center">
-                        <button id="okButton" class=" btn btn-secondary">Ok</button>
-                    </div>
-                </div>
-        `;
+        addEvent(messageBoxDiv, 'click', () => messageBoxDiv.style.zIndex = nextZIndex++);
 
-        messageBoxDiv.innerHTML = messageBoxHtml;
+        const boxHeading = document.createElement('h2');
+        boxHeading.innerHTML = 'Alert';
+        const message = document.createElement('span');
+        message.innerText = msg;
+
+        const buttonDiv = document.createElement('div');
+        buttonDiv.classList.add('buttonDiv');
+        
+        if(Array.isArray(options)){
+            options.forEach((element) => {
+                const newButton = document.createElement('button');
+                let textToDispaly;
+                if (typeof element === 'string' || element instanceof String){
+                    //Slice the string if the option if bigger than 20 characters
+                    textToDispaly = element.slice(0, 20);
+                } else {
+                    textToDispaly = 'unknown';
+                }
+                newButton.innerText = textToDispaly;
+                addEvent(newButton, 'click', () => {
+                    messageBoxDiv.remove();
+                    model.style.display = 'none';
+                    if(callback){
+                        callback(textToDispaly)
+                    }
+                });
+                buttonDiv.appendChild(newButton)
+            });
+        } else {
+            const okButton = document.createElement('button');
+            okButton.innerText = 'ok';
+            buttonDiv.appendChild(okButton); 
+
+            addEvent(okButton, 'click', () => {
+                messageBoxDiv.remove();
+                model.style.display = 'none';
+            });
+        }
+        
+
+        messageBoxDiv.appendChild(boxHeading);
+        messageBoxDiv.appendChild(message);
+        messageBoxDiv.appendChild(buttonDiv);
+
         document.body.appendChild(messageBoxDiv);
+
+        topOffset += offsetIncrement;
+        leftOffset += offsetIncrement;
+
+
+        let topPosition = parseFloat(getComputedStyle(messageBoxDiv).top);
+        let leftPosition = parseFloat(getComputedStyle(messageBoxDiv).left)
+        
+        if (topPosition + topOffset + height > window.innerHeight) {
+            topOffset -= window.innerHeight - height;
+        }
+    
+        if (leftPosition + leftOffset + width > window.innerWidth) {
+            leftOffset -= window.innerWidth - width;
+        }
+
     }
 
-    makeMessageBox("Hello world");
-
-    addEvent(bidenAlert, 'click', () => {
-        window.messageBox.show('Due to the pandamic all employeess that are not vecenaied will be fired on spot, and send to jail for 5 months, and will require to pay a fine of $100,000 to the Binden administrator, texes and fee are not included.');
-    });
-
-    addEvent(trumpAlert, 'click', () => {
-        window.messageBox.show('We will never give up!!!');
-    });
-
-    addEvent(get('okButton'), 'click', () => {
-        window.messageBox.hide();
-    });
-
-    addEvent(get('customAlert'), 'submit', (event) => {
-        event.preventDefault();
-        let alertText = get('customAlertText');
-        window.messageBox.show(alertText.value);
-        alertText.value = '';
-    });
-
-
     return {
-        show: 'showMessageBox'
+        show: showMessageBox
     };
 })();
